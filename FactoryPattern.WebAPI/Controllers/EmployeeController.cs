478ebcf2f67dd75a5f96543c63fb27f03b6c92c8
KurnakovMaksim <maksk210@gmail.com>
2021-11-18 11:36:20 +0900

    Add WebAPI

---
+++ FactoryPattern.WebAPI/Controllers/EmployeeController.cs
@@ -0,0 +1,30 @@
+﻿using FactoryPattern.BL.Employees.Abstract;
+using FactoryPattern.BL.Factories.Employees;
+using FactoryPattern.WebAPI.DTOs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FactoryPattern.WebAPI.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class EmployeeController : ControllerBase
+{
+    private readonly ICollection<IEmployee> _employees = new List<IEmployee>();
+    private IEmployeeFactory _employeeFactory;
+
+    [HttpGet]
+    public ActionResult<ICollection<IEmployee>> Get()
+    {
+        return Ok(_employees);
+    }
+
+    [HttpPost]
+    public ActionResult<IEmployee> Create(EmployeeDTO employeeDTO)
+    {
+        _employeeFactory = new EmployeeFactory(employeeDTO.Name, employeeDTO.Salary);
+        var employee = _employeeFactory.Create(employeeDTO.Type);
+        _employees.Add(employee);
+
+        return Ok(employee);
+    }
+}
---
+++ FactoryPattern.WebAPI/DTOs/EmployeeDTO.cs
@@ -0,0 +1,11 @@
+﻿
+using FactoryPattern.BL.Factories.Employees;
+
+namespace FactoryPattern.WebAPI.DTOs;
+
+public class EmployeeDTO
+{
+    public string Name { get; set; }
+    public int Salary { get; set; }
+    public EmployeeType Type { get; set; }
+}
